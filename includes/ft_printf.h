a6446e3b0e3277dd9a56749462db2774e4e594c0
Bilal AL-KHANI <bal-khan@e1r9p2.42.fr>
2018-03-10 00:22:04 +0100

    ajout attribute format

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jbulant <jbulant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/11 22:05:12 by jbulant           #+#    #+#             */
-/*   Updated: 2018/03/10 00:15:30 by jbulant          ###   ########.fr       */
+/*   Updated: 2018/03/10 00:20:54 by bal-khan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <stdarg.h>
 # include "libft.h"
 
-int		ft_printf(const char *format, ...);
+# define A __attribute__ ((format (printf, 1, 2)))
+int	ft_printf(const char *format, ...) A;
+# undef A
 
 #endif
